630fba5f1844a8374e1ec2dcfbaa54a95e91ef77
Alexander Hadik <alex@alexhadik.com>
2016-06-04 14:04:56 -0500

    Adding routes file (#2)
    Passport oauth module
    
    Adding routes

---
+++ modules/config/auth.js
@@ -0,0 +1,10 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : process.env.FACEBOOK_CLIENT_ID, // your App ID
+        'clientSecret'  : process.env.FACEBOOK_CLIENT_SECRET, // your App Secret
+        'callbackURL'   : process.env.TABULOUS_FACEBOOK_CALLBACK,
+        'enableProof' : true
+    }
+};
