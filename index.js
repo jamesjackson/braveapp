b678df4c7826abdce80f064577f741101fd87674
jamesjackson <fiberhog@gmail.com>
2016-06-04 13:16:13 -0500

    initial commit - test

---
+++ index.js
@@ -0,0 +1,26 @@
+var restify = require('restify');
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+    next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+server.use(restify.CORS());
+
+
+// server.get(/\/?.*/, restify.serveStatic({
+//     default: 'login.html',
+//     directory: './public'
+// }));
+
+server.listen(process.env.PORT || 8080, function() {
+    return console.log('%s listening at %s', server.name, server.url);
+});
