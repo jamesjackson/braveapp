daac78e4f40e9df50a6fde71fb069cd4493b88bc
Una Kravets <una.kravets@gmail.com>
2016-06-04 15:34:17 -0500

    trying to make oauth work (continuation)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var restify = require('restify');
-var passport = require('passport-restify');
+var passport = require('passport');
 var mongoose = require('mongoose');
+
+require('./modules/config/passport')(passport);
 
 function respond(req, res, next) {
     res.send('hello ' + req.params.name);
@@ -11,11 +13,13 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-
 server.use(restify.CORS());
+server.use(restify.queryParser());
+server.use(passport.initialize());
+server.use(passport.session());
 
-require('./modules/config/routes.js')(server, restify, null);
-require('./modules/config/passport')(passport);
+require('./modules/config/routes.js')(server, restify, passport);
+
 
 server.listen(process.env.PORT || 8080, function() {
     return console.log('%s listening at %s', server.name, server.url);
