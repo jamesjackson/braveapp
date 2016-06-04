333357c6e371a83349e4dc28b4719a8ed53df584
ahadik <ahadik@us.ibm.com>
2016-06-04 16:29:11 -0500

    updated db authenticated

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 require('./modules/config/passport')(passport);
 
-var mongoCreds = {
-	user : 'braveapp',
-	password : '123456',
-	uri : 'aws-us-east-1-portal.11.dblayer.com',
-	port : '28113',
-	database : 'brave-mongo'
-}
-var dbURL = "mongodb://"+mongoCreds.user+":"+mongoCreds.password+"@"+mongoCreds.uri+":"+mongoCreds.port+"/"+mongoCreds.database+"?ssl=true";
+var dbURL = process.env.MONGOHQ_URL;
 
 function respond(req, res, next) {
     res.send('hello ' + req.params.name);
@@ -25,19 +18,9 @@ server.head('/hello/:name', respond);
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
-
-var ca =[fs.readFileSync('private/cert.pem')];
-var options = {
-	mongos: {
-		ssl: true,
-		sslValidate: true,
-		sslCA: ca // cert from compose.io dashboard
-	}
-}
-
-mongoose.connect(dbURL, options); // connect to our database
+mongoose.connect(dbURL); // connect to our database
 mongoose.connection.on('error', function (err) {
- console.log('error');
+ console.log(err);
 });
 
 server.use(restify.CORS());
