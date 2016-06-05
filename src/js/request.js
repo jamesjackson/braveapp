73ad86e0080c1f05816d8d8a86a6fd5dc28cf12a
Una Kravets <una.kravets@gmail.com>
2016-06-05 07:46:13 -0500

    better reward redemption

--- src/js/request.js
+++ src/js/request.js
@@ -1,21 +1,36 @@
 var data;
+var userPoints = document.querySelector('.user--points');
 
 function reqListener () {
   data =  JSON.parse(this.responseText);
+  buildProfile();
+}
+
+// build profile
+function buildProfile () {
   var userName = document.querySelector('.user--name');
-  var userPoints = document.querySelector('.user--points');
   var userPhoto = document.querySelector('.user--photo');
 
   // user profile
   userName.innerHTML = data.user;
   userPoints.innerHTML = 'Total Points:' + data.points;
   userPhoto.innerHTML = '<img src="http://' + data.photo + '" />';
-}
+};
 
 //points
-function redeemPoints (n) {
-  var userPoints = document.querySelector('.user--points');
-  data.points -= n;
+function redeemPoints (n, reward) {
+  var answer = window.confirm('Are you sure you want to redeem ' + reward + ' for ' + n + ' points?');
+    if (answer) {
+      data.points -= n;
+      userPoints.innerHTML = 'Total Points:' + data.points;
+    }
+    else {
+    alert ("No Points Were Charged.")
+  }
+}
+
+function earnPoints (n) {
+  data.points += n;
   userPoints.innerHTML = 'Total Points:' + data.points;
 }
 
