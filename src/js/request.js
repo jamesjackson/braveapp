652857f40ac9022f9ad0a2b9b097306d39e7a941
Una Kravets <una.kravets@gmail.com>
2016-06-04 21:59:17 -0500

    redeem points

--- src/js/request.js
+++ src/js/request.js
@@ -1,12 +1,22 @@
+var data;
+
 function reqListener () {
-  var data =  JSON.parse(this.responseText);
+  data =  JSON.parse(this.responseText);
   var userName = document.querySelector('.user--name');
   var userPoints = document.querySelector('.user--points');
   var userPhoto = document.querySelector('.user--photo');
 
+  // user profile
   userName.innerHTML = data.user;
   userPoints.innerHTML = 'Total Points:' + data.points;
   userPhoto.innerHTML = '<img src="http://' + data.photo + '" />';
+}
+
+//points
+function redeemPoints (n) {
+  var userPoints = document.querySelector('.user--points');
+  data.points -= n;
+  userPoints.innerHTML = 'Total Points:' + data.points;
 }
 
 var oReq = new XMLHttpRequest();
